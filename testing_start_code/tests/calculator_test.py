8109442e6132525753d1836ffcfd568e6e98946a
milacerveira <milascerveira@gmail.com>
2020-11-24 19:32:03 +0000

    remove some text

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -3,9 +3,6 @@ from src.calculator import add, divide, multiply, subtract
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(5, add(2, 3))
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(5, add(2, 3))
 
     def test_subtract(self):
         self.assertEqual(3, subtract(10, 7))
